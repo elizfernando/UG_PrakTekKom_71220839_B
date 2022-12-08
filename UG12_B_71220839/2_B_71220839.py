4501859c4f5eb60c332c5c69b5c627ac52d6de6e
elizfernando <118380082+elizfernando@users.noreply.github.com>
2022-12-08 12:57:45 +0700

    Add files via upload

---
+++ UG12_B_71220839/2_B_71220839.py
@@ -0,0 +1,4 @@
+print("~~~ Selamat Datang di Kalkulator Sederhana ~~~")
+y = int(input("Masukkan operator matematika yang ingin Anda hitung; "))
+g = int(input("Mau penjumlahan berapa; "))
+k = int(input("Print berapa banyak: "))
